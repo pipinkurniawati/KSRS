b4ce831f86dc6e522395a7b3b6f634d03af3c4f3
christchron <randi_chilz@yahoo.com>
2016-04-15 13:18:29 +0700

    added login navbar

--- navigation_bar.php
+++ navigation_bar.php
@@ -10,8 +10,13 @@
               <li class="<?php if(basename($_SERVER['SCRIPT_NAME']) == 'form_daftar.php')?>"><a style="color:white;" href="form_daftar.php"><h6><strong>Pendaftaran</strong></h6></a></li>
               <li class="<?php if(basename($_SERVER['SCRIPT_NAME']) == 'pengumuman.php')?>"><a style="color:white;" href="pengumuman.php"><h6><strong>Pengumuman</strong></h6></a></li>
               <li class="<?php if(basename($_SERVER['SCRIPT_NAME']) == 'persyaratan.php')?>"><a style="color:white;" href="persyaratan.php"><h6><strong>Persyaratan</strong></h6></a></li>
-              <li class="<?php if(basename($_SERVER['SCRIPT_NAME']) == 'jadwal_siar.php')?>"><a style="color:white;" href="jadwal_siar.php"><h6><strong>Jadwal Siar</strong></h6></a></li>
-            </ul>
+              <li class="<?php if(basename($_SERVER['SCRIPT_NAME']) == 'jadwal_siar.php')?>"><a style="color:white;" href="jadwal_siar.php"><h6><strong>Jadwal Siar</strong></h6></a></li>            
+          </ul>
+          
+          <ul class="light-blue lighten-1 nav navbar-nav" style="float:right;">
+              <li class="<?php if(basename($_SERVER['SCRIPT_NAME']) == 'login.php')?>"><a style="color:white;" href="login.php"><h6><strong>Login</strong></h6></a></li>
+          </ul>
+          
         </div>
       </div>
     </nav>
