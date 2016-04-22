25a757a67fe1262e06481ff61b96fed54c9664ef
pipinkurniawati <13513089@std.stei.itb.ac.id>
2016-04-22 15:49:05 +0700

    merge

--- navigation_bar.php
+++ navigation_bar.php
@@ -24,7 +24,7 @@
                 
                 echo'
                   <ul class="light-blue lighten-1 nav navbar-nav">
-                    <li class="'; if(basename($_SERVER['SCRIPT_NAME']) == 'pengumuman.php'); echo'"><a style="color:white;" href="pengumuman.php?token='.$token.'"><h6><strong>Pengumuman</strong></h6></a></li>
+                    <li class="'; if(basename($_SERVER['SCRIPT_NAME']) == 'daftar_calon.php'); echo'"><a style="color:white;" href="daftar_calon.php?token='.$token.'"><h6><strong>Calon Penyiar</strong></h6></a></li>
                     <li class="'; if(basename($_SERVER['SCRIPT_NAME']) == 'persyaratan.php'); echo'"><a style="color:white;" href="persyaratan.php?token='.$token.'"><h6><strong>Persyaratan</strong></h6></a></li>
                     <li class="'; if(basename($_SERVER['SCRIPT_NAME']) == 'jadwal_siar_admin.php'); echo'"><a style="color:white;" href="jadwal_siar_admin.php?token='.$token.'"><h6><strong>Jadwal Siar</strong></h6></a></li>            
                   </ul>';
